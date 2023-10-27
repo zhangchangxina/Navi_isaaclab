zhangchangxina/Navi_isaaclab

commit 850cf045946ee9eeac10df56d184816f8659e576

Fixes training of anymal rough terrain locomotion (#207)
# Description

Fixes the environment standalone scripts and rough terrain locomotion
training.


## Type of change

- Bug fix (non-breaking change which fixes an issue)


## Checklist

- [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
`./orbit.sh --format`
- [ ] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [x] I have updated the changelog and the corresponding version in the
extension's `config/extension.toml` file

source/extensions/omni.isaac.orbit/config/extension.toml
@@ -1,7 +1,7 @@
 [package]
 
 # Note: Semantic Versioning is used: https://semver.org/
-version = "0.9.20"
+version = "0.9.21"
 
 # Description
 title = "ORBIT framework for Robot Learning"

source/extensions/omni.isaac.orbit/omni/isaac/orbit/assets/asset_base.py
@@ -56,16 +56,19 @@ class AssetBase(ABC):
 
         # note: Use weakref on all callbacks to ensure that this object can be deleted when its destructor is called.
         # add callbacks for stage play/stop
+        # The order is set to 10 which is arbitrary but should be lower priority than the default order of 0
         physx_interface = omni.physx.acquire_physx_interface()
         self._initialize_handle = physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
             int(omni.physx.bindings._physx.SimulationEvent.RESUMED),
             lambda event, obj=weakref.proxy(self): obj._initialize_callback(event),
+            order=10,
         )
         self._invalidate_initialize_handle = (
             physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
                 int(omni.physx.bindings._physx.SimulationEvent.STOPPED),
                 lambda event, obj=weakref.proxy(self): obj._invalidate_initialize_callback(event),
-            )
+                order=10,
+            ),
         )
         # add callback for debug visualization
         if self.cfg.debug_vis:

source/extensions/omni.isaac.orbit/omni/isaac/orbit/sensors/sensor_base.py
@@ -53,16 +53,19 @@ class SensorBase(ABC):
 
         # note: Use weakref on callbacks to ensure that this object can be deleted when its destructor is called.
         # add callbacks for stage play/stop
+        # The order is set to 10 which is arbitrary but should be lower priority than the default order of 0
         physx_interface = omni.physx.acquire_physx_interface()
         self._initialize_handle = physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
             int(omni.physx.bindings._physx.SimulationEvent.RESUMED),
             lambda event, obj=weakref.proxy(self): obj._initialize_callback(event),
+            order=10,
         )
         self._invalidate_initialize_handle = (
             physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
                 int(omni.physx.bindings._physx.SimulationEvent.STOPPED),
                 lambda event, obj=weakref.proxy(self): obj._invalidate_initialize_callback(event),
-            )
+                order=10,
+            ),
         )
         # add callback for debug visualization
         if self.cfg.debug_vis:

source/extensions/omni.isaac.orbit/omni/isaac/orbit/envs/mdp/observations.py
@@ -75,9 +75,8 @@ def height_scan(env: BaseEnv, sensor_cfg: SceneEntityCfg) -> torch.Tensor:
     """Height scan from the given sensor w.r.t. the sensor's frame."""
     # extract the used quantities (to enable type-hinting)
     sensor: RayCaster = env.scene.sensors[sensor_cfg.name]
-    # return the height scan
-    heights = sensor.data.pos_w[:, 2].unsqueeze(1) - sensor.data.ray_hits_w[..., 2] - 0.5
-    return heights
+    # height scan: height = sensor_height - hit_point_z - 0.5
+    return sensor.data.pos_w[:, 2].unsqueeze(1) - sensor.data.ray_hits_w[..., 2] - 0.5
 
 
 """

source/extensions/omni.isaac.orbit/omni/isaac/orbit/envs/rl_env.py
@@ -329,10 +329,10 @@ class RLEnv(BaseEnv, gym.Env):
         """
         # update the curriculum for environments that need a reset
         self.curriculum_manager.compute(env_ids=env_ids)
-        # reset the internal buffers of the scene elements
-        self.scene.reset(env_ids)
         # randomize the MDP for environments that need a reset
         self.randomization_manager.randomize(env_ids=env_ids, mode="reset")
+        # reset the internal buffers of the scene elements
+        self.scene.reset(env_ids)
 
         # iterate over all managers and reset them
         # this returns a dictionary of information which is stored in the extras

source/extensions/omni.isaac.orbit/omni/isaac/orbit/managers/action_manager.py
@@ -215,7 +215,7 @@ class ActionManager(ManagerBase):
         """
         # check if action dimension is valid
         if self.total_action_dim != action.shape[1]:
-            raise ValueError(f"Invalid action shape, expected: {self.total_action_dim}, received: {action.shape[1]}")
+            raise ValueError(f"Invalid action shape, expected: {self.total_action_dim}, received: {action.shape[1]}.")
         # store the input actions
         self._prev_action[:] = self._action
         self._action[:] = action.to(self.device)

source/extensions/omni.isaac.orbit/omni/isaac/orbit/managers/observation_manager.py
@@ -123,7 +123,10 @@ class ObservationManager(ManagerBase):
         If a corruption/noise model is registered for a term, the function is called to corrupt
         the observation. The corruption function is expected to return a tensor with the same
         shape as the observation. The observations are clipped and scaled as per the configuration
-        settings. By default, no scaling or clipping is applied.
+        settings.
+
+        The operations are performed in the order: compute, add corruption/noise, clip, scale.
+        By default, no scaling or clipping is applied.
 
         Args:
             group_name: The name of the group for which to compute the observations. Defaults to :obj:`None`,

source/extensions/omni.isaac.orbit/omni/isaac/orbit/sensors/contact_sensor/contact_sensor.py
@@ -286,6 +286,10 @@ class ContactSensor(SensorBase):
         if self.contact_visualizer is None:
             visualizer_cfg = CONTACT_SENSOR_MARKER_CFG.replace(prim_path="/Visuals/ContactSensor")
             self.contact_visualizer = VisualizationMarkers(visualizer_cfg)
+        # safely return if view becomes invalid
+        # note: this invalidity happens because of isaac sim view callbacks
+        if self.body_physx_view is None:
+            return
         # marker indices
         # 0: contact, 1: no contact
         net_contact_force_w = torch.norm(self._data.net_forces_w, dim=-1)

commit 2046ec7c4ad4501b50ca9246d0671466a2903adb

Fixes typo in constructor for physics events (#217)
# Description

This MR fixes a bug introduced in #207 which was assigning tuples to the
`invalidate_handle` member in `AssetBase` and `SensorBase`.

## Type of change

<!-- As you go through the list, delete the ones that are not
applicable. -->

- Bug fix (non-breaking change which fixes an issue)

## Checklist

- [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
`./orbit.sh --format`
- [ ] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [x] I have updated the changelog and the corresponding version in the
extension's `config/extension.toml` file

source/extensions/omni.isaac.orbit/config/extension.toml
@@ -1,7 +1,7 @@
 [package]
 
 # Note: Semantic Versioning is used: https://semver.org/
-version = "0.9.22"
+version = "0.9.23"
 
 # Description
 title = "ORBIT framework for Robot Learning"

source/extensions/omni.isaac.orbit/omni/isaac/orbit/assets/asset_base.py
@@ -57,18 +57,16 @@ class AssetBase(ABC):
         # note: Use weakref on all callbacks to ensure that this object can be deleted when its destructor is called.
         # add callbacks for stage play/stop
         # The order is set to 10 which is arbitrary but should be lower priority than the default order of 0
-        physx_interface = omni.physx.acquire_physx_interface()
-        self._initialize_handle = physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
+        physx_event_stream = omni.physx.acquire_physx_interface().get_simulation_event_stream_v2()
+        self._initialize_handle = physx_event_stream.create_subscription_to_pop_by_type(
             int(omni.physx.bindings._physx.SimulationEvent.RESUMED),
             lambda event, obj=weakref.proxy(self): obj._initialize_callback(event),
             order=10,
         )
-        self._invalidate_initialize_handle = (
-            physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
-                int(omni.physx.bindings._physx.SimulationEvent.STOPPED),
-                lambda event, obj=weakref.proxy(self): obj._invalidate_initialize_callback(event),
-                order=10,
-            ),
+        self._invalidate_initialize_handle = physx_event_stream.create_subscription_to_pop_by_type(
+            int(omni.physx.bindings._physx.SimulationEvent.STOPPED),
+            lambda event, obj=weakref.proxy(self): obj._invalidate_initialize_callback(event),
+            order=10,
         )
         # add callback for debug visualization
         if self.cfg.debug_vis:

source/extensions/omni.isaac.orbit/omni/isaac/orbit/sensors/sensor_base.py
@@ -54,18 +54,16 @@ class SensorBase(ABC):
         # note: Use weakref on callbacks to ensure that this object can be deleted when its destructor is called.
         # add callbacks for stage play/stop
         # The order is set to 10 which is arbitrary but should be lower priority than the default order of 0
-        physx_interface = omni.physx.acquire_physx_interface()
-        self._initialize_handle = physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
+        physx_event_stream = omni.physx.acquire_physx_interface().get_simulation_event_stream_v2()
+        self._initialize_handle = physx_event_stream.create_subscription_to_pop_by_type(
             int(omni.physx.bindings._physx.SimulationEvent.RESUMED),
             lambda event, obj=weakref.proxy(self): obj._initialize_callback(event),
             order=10,
         )
-        self._invalidate_initialize_handle = (
-            physx_interface.get_simulation_event_stream_v2().create_subscription_to_pop_by_type(
-                int(omni.physx.bindings._physx.SimulationEvent.STOPPED),
-                lambda event, obj=weakref.proxy(self): obj._invalidate_initialize_callback(event),
-                order=10,
-            ),
+        self._invalidate_initialize_handle = physx_event_stream.create_subscription_to_pop_by_type(
+            int(omni.physx.bindings._physx.SimulationEvent.STOPPED),
+            lambda event, obj=weakref.proxy(self): obj._invalidate_initialize_callback(event),
+            order=10,
         )
         # add callback for debug visualization
         if self.cfg.debug_vis:

source/extensions/omni.isaac.orbit/test/assets/check_external_force.py
@@ -115,7 +115,7 @@ def main():
         robot.set_joint_position_target(robot.data.default_joint_pos.clone())
         robot.write_data_to_sim()
         # perform step
-        sim.step(render=app_launcher.RENDER)
+        sim.step()
         # update sim-time
         sim_time += sim_dt
         count += 1

source/extensions/omni.isaac.orbit/test/assets/test_articulation.py
@@ -147,7 +147,7 @@ class TestArticulation(unittest.TestCase):
                 robot.set_joint_position_target(robot.data.default_joint_pos.clone())
                 robot.write_data_to_sim()
                 # perform step
-                self.sim.step(render=app_launcher.RENDER)
+                self.sim.step()
                 # update buffers
                 robot.update(self.dt)
             # check condition that the robots have fallen down
@@ -194,7 +194,7 @@ class TestArticulation(unittest.TestCase):
                 robot.set_joint_position_target(robot.data.default_joint_pos.clone())
                 robot.write_data_to_sim()
                 # perform step
-                self.sim.step(render=app_launcher.RENDER)
+                self.sim.step()
                 # update buffers
                 robot.update(self.dt)
             # check condition